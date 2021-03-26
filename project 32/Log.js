a96fa7d37f06aa94a1c8c942bcd91fca0fa6ab92
Paranjay Sharma <76864845+Paranjay-Sharma@users.noreply.github.com>
2021-03-26 15:40:50 +0530

    Add files via upload

---
+++ project 32/Log.js
@@ -0,0 +1,25 @@
+class Log extends BaseClass{
+  constructor(x,y,height,angle){
+    super(x,y,40,height,angle);
+    this.image = loadImage("sprites/wood2.png");
+    Matter.Body.setAngle(this.body, angle);
+  }
+
+  display(){
+    //console.log(this.body.speed);
+    if(this.body.speed < 10){
+     super.display();
+    }
+    else{
+      World.remove(world, this.body);
+      push();
+      this.Visiblity = this.Visiblity - 5;
+      tint(255,this.Visiblity);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      pop();
+    }
+    
+  }
+
+
+}
